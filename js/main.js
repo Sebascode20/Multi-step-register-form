c1d314112cf74745c250ac5bc42e4d2fb5980975
Sebascode20 <sebastianemiliotv@gmail.com>
2026-05-20 17:43:15 -0600

    Add form structure and styles

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const d = document;
+
+const $continueBtnRegister = d.getElementById("register-btn"),
+$continueBtnTopics = d.getElementById("topics-btn"),
+$confirmBtnSummary = d.getElementById("btn-confirm"),
+$registerForm = d.querySelector(".register"),
+$topicsForm = d.querySelector("topics");
+
